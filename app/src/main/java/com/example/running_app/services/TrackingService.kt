5fc5558a9dc915d0329426d0acb5f8d3cd90763d
Jorge Arturo Rodriguez Martin <joarceus@hotmail.com>
2020-08-29 17:59:52 -0500

    Creation of the structure of the service and launch management

--- app/src/main/java/com/example/running_app/services/TrackingService.kt
+++ app/src/main/java/com/example/running_app/services/TrackingService.kt
@@ -1,22 +1,56 @@
 package com.example.running_app.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
-import com.example.running_app.utils.ACTION_PAUSE_SERVICE
-import com.example.running_app.utils.ACTION_START_OR_RESUME_SERVICE
-import com.example.running_app.utils.ACTION_STOP_SERVICE
+import com.example.running_app.R
+import com.example.running_app.ui.MainActivity
+import com.example.running_app.utils.*
 import timber.log.Timber
 
 // inherits from lifeCycleService instead of Service because needs to be observed from other parts
 class TrackingService : LifecycleService() {
 
+    //SERVICES
+    /*Foreground
+    Can't be killed by the android system
+    A foreground service performs some operation that is noticeable to the user.
+    For example, an audio app would use a foreground service to play an audio track.
+    Foreground services must display a Notification. Foreground services continue running
+    even when the user isn't interacting with the app.*/
+
+/*  Background
+    Can't be killed by the android system
+    A background service performs an operation that isn't
+    directly noticed by the user. For example, if an app used a service to compact its
+    storage, that would usually be a background service.*/
+
+
+    var _isFirstRun = true
+
     //part of the lifecycle of services, it starts the service
     //This is called when a component like an activity calls to start or do something with the service
+    //in this case called by the button of runFragment
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when(it.action){
                 ACTION_START_OR_RESUME_SERVICE -> {
-                    Timber.d("Started or resumed service")
+                    if (_isFirstRun){
+                        startForegroundService()
+                        _isFirstRun = false
+                        Timber.d("Started service")
+                    }
+                    else{
+                        Timber.d("Resumed service")
+                    }
                 }
                 ACTION_PAUSE_SERVICE -> {
                     Timber.d("Paused service")
@@ -27,5 +61,51 @@ class TrackingService : LifecycleService() {
             }
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForegroundService(){
+        // a service from android OS that we need in order to show a notification
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        //Channel is only needed for Oreo or newer
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            //we are on android Oreo or later
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false) //not disappear when touching
+            .setOngoing(true) // can't be swiped away
+            .setSmallIcon(R.drawable.ic_run)
+            .setContentTitle("Running app")
+            .setContentText("00:00:00")
+            .setContentIntent(getMainActivityPendingIntent()) // declares what to do or where to go when the
+            //notification is touched
+
+        //Starts the service as foreground with the prev config
+        startForeground(NOTIFICATION_ID,notificationBuilder.build())
+    }
+
+    //To define what to do when touching the notification
+    private fun getMainActivityPendingIntent() =
+        PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this,MainActivity::class.java).also {
+                it.action = ACTION_SHOW_TRACKING_FRAGMENT // constant to control what action is needed with the intent
+            },
+            FLAG_UPDATE_CURRENT // if the intent already existed it will update it instead of
+        // creating a new one
+        )
+
+    //Needs Oreo or newer
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager : NotificationManager){
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
     }
 }
--- app/src/main/java/com/example/running_app/ui/MainActivity.kt
+++ app/src/main/java/com/example/running_app/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.running_app.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.running_app.R
+import com.example.running_app.utils.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -21,6 +23,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //if the activity was destroyed but the service is running and the user touches it
+        // then onCreate of this activity will be called and do this
+        navigateToTrackingFragmentIfNeeded(intent)
 
         //Our custom toolBar
         setSupportActionBar(toolbar)
@@ -37,5 +43,18 @@ class MainActivity : AppCompatActivity() {
                     else -> bottomNavigationView.visibility = View.GONE
                 }
             }
+    }
+
+    //if the service notification was touched but the activity is not destroyed
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?){
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT){ // the intent action made for us
+            //means was launched by the notification click
+            navHostFragment.findNavController().navigate(R.id.action_global_tracking_fragment)
+        }
     }
 }
